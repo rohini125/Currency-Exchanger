5270823e0ff8ba6d99da67aafd993055f79707fc
sakshi parkale <120358706+SakshiParkale@users.noreply.github.com>
2024-08-09 19:01:01 +0530

    import register page

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import './stylesheets/alignments.css'
-import './stylesheets/custom-components.css'
-import './stylesheets/form-elements.css'
-import './stylesheets/text-elements.css'
-import './stylesheets/theme.css'
+import './Stylesheets/alignments.css'
+import './Stylesheets/custom-components.css'
+import './Stylesheets/form-elements.css'
+import './Stylesheets/text-elements.css'
+import './Stylesheets/theme.css'
 import Login from './pages/Login/Login.js'
-import Register from './pages/Register/Register';
+import Register from './pages/Register/Register.js';
 
 
 function App() {
