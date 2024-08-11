0a818a08e478e72fc3f97690127d0016456949d8
sakshi parkale <120358706+SakshiParkale@users.noreply.github.com>
2024-08-11 17:26:38 +0530

    adding login page

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -1,13 +1,10 @@
+
 import React from 'react';
-import { useState } from 'react';
-import { Col, Form, Row } from "antd";
-import email_icon from '../../Assets/email_icon.png'
-import password_icon from '../../Assets/password_icon.png';
-import user_icon from '../../Assets/user_icon.png'
+import { Col, Form, Row , Input} from "antd";
+import {useNavigate} from "react-router-dom";
 
 function Login() {
-    const [action, setAction] = useState("Login")
-
+    const navigate = useNavigate();
     const onFinish = (values) => {
         console.log("Received values of form: ", values);
     };
@@ -16,32 +13,31 @@ function Login() {
         <div className="bg-primary flex items-center justify-center h-screen">
             <div className="card w-400 p-2">
                 <div className="flex items-center justify-between">
-                    <h1 className="text-primary items-center gap-1">Login</h1></div>
-                <hr />
-                <Form layout="vertical" onFinish={onFinish} >
-                    <div className='inputs'>
-                        <div className='input'>
-                            <img className='mlr-3 m-0' src={user_icon} alt="user_icon" />
-                            <input type='text' placeholder='User name' />
-                        </div>
-                        <div className='input'>
-                            <img className='mlr-3 m-0' src={email_icon} alt="email_icon" />
-                            <input type='email' placeholder='Email' />
-                        </div>
-                        <div className='input'>
-                            <img className='mlr-3 m-0' src={password_icon} alt="password_icon" />
-                            <input type='password' placeholder='Password' />
-                        </div>
-                    </div>
+                    <h1 className="text-2xl">Payment-Login</h1>
+                </div>
+                 <hr />
+                <Form layout="vertical" onFinish={onFinish}>
+                    <Row gutter={16}>  
+                        
+                        <Col span={24}>
+                            <Form.Item label="Email" name="email">
+                                <Input type="text"/>
+                            </Form.Item>
+                        </Col>
+                        
+                        <Col span={24}> 
+                            <Form.Item label="Password" name="password">
+                                <Input type="password"/>
+                            </Form.Item>
+                        </Col>
+                    </Row> 
 
-                    <div className=' forget-password m-2 fs-1 text-third'>
-                        Forget password  <span >click here !</span>
-                    </div>
-
-
-                    <div className="submit bg-primary justify-center items-center " onClick={()=>{setAction("Login")}}>Log In</div>
-
-
+                        <button className="primary-contained-btn w-100" type="submit">
+                            Login
+                        </button>
+                        <h1 className="text-sm underline mt-2" onClick={()=> navigate("/register")}>
+                        Already a member ,Click Here To Register
+                        </h1>
                 </Form>
             </div>
         </div>
@@ -49,3 +45,5 @@ function Login() {
 }
 
 export default Login;
+
+
