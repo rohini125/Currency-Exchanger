87f314840fe3f7fefd881d8f7a3c126b85652b85
sakshi parkale <120358706+SakshiParkale@users.noreply.github.com>
2024-09-30 22:55:41 +0530

    import pages folder files

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,21 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Register from "./pages/Register/Register.js";
-import Login from "./pages/Login/Login.js";
+import Register from "./Pages/Register.js";
+import Login from "./Pages/Login.js";
+import Header from "./Pages/Header.js";
+import Footer from "./Pages/Footer.js";
 
 function App() {
   return (
+    <>
+    <Header/>
     <Routes>
       <Route path="/register" element={<Register />} />
       <Route path="/login" element={<Login />} />
       <Route path="/payment" element={<div>Payment Page Placeholder</div>} />
     </Routes>
+    <Footer/>
+    </>
   );
 }
 
