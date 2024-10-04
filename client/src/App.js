35e05592a5e3d1ae6dc1dba11113b60c7d64026d
sakshi parkale <120358706+SakshiParkale@users.noreply.github.com>
2024-10-04 16:35:28 +0530

    import markets,notification,HelpPage,dropdown,currencyConverter file in App.js file

--- client/src/App.js
+++ client/src/App.js
@@ -4,17 +4,22 @@ import Register from "./Pages/Register.js";
 import Login from "./Pages/Login.js";
 import Header from "./Pages/Header.js";
 import Footer from "./Pages/Footer.js";
-import Home from "./Pages/Home.js";
+import CurrencyConverter from "./Pages/currency-converter.js";
+import Markets from "./Pages/Markets.js";
+import HelpPage from "./Pages/HelpPage.js";
+import Notifications from "./Pages/Notifications.js"
 
 function App() {
   return (
-    <div className="bg-slate-400 h-full w-full">
+    <div className="bg-gray-50 h-full w-full">
     <Header/>
     <Routes>
       <Route path="/register" element={<Register />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/" element={<Home />} />
-      {/* <Route path="/payment" element={<div>Payment Page Placeholder</div>} /> */}
+      <Route path="/" element={<CurrencyConverter />} />
+      <Route path="/markets" element={<Markets />} />
+      <Route path="/help" element={<HelpPage />} />
+      <Route path="/notifications" element={<Notifications />} />
     </Routes>
     <Footer/>
     </div>
@@ -27,3 +32,59 @@ export default App;
 
 
 
+// import React from "react";
+// import { Link } from 'react-router-dom';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; // Import FontAwesomeIcon
+// import { faUser, faCreditCard, faHandHoldingDollar, faLandmark } from '@fortawesome/free-solid-svg-icons'; // Import icons
+
+// const Home = () => {
+//   return (
+//     <div className="container mx-auto p-4">
+//       <div className="flex justify-center items-center text-4xl mb-4">
+//         <span>Transfer Money</span>
+//       </div>
+//       <div className="flex flex-wrap justify-around mb-4">
+//         <div className="w-full md:w-1/2 lg:w-1/5 xl:w-1/5 p-2">
+//           <div className="bg-slate-50 h-48 flex items-center justify-center m-2 rounded-lg shadow-md hover:shadow-lg transition duration-300 hover:bg-slate-200 hover:text-blue-500">
+//             <div className="text-center">
+//               <Link to="/">
+//                 <FontAwesomeIcon icon={faUser} size="2x" />
+//                 <div className="text-xl items-center mt-2">To Mobile Number</div>
+//               </Link>
+//             </div>
+//           </div>
+//         </div>
+//         <div className="w-full md:w-1/2 lg:w-1/5 xl:w-1/5 p-2">
+//           <div className="bg-slate-50 h-48 flex items-center justify-center m-2 rounded-lg shadow-md hover:shadow-lg transition duration-300 hover:bg-slate-200 hover:text-blue-500">
+//             <div className="text-center">
+//               <Link to="/">
+//                 <FontAwesomeIcon icon={faLandmark} size="2x" />
+//                 <div className="text-xl items-center mt-2">To Bank/UPI ID</div>
+//               </Link>
+//             </div>
+//           </div>
+//         </div>
+//         <div className="w-full md:w-1/2 lg:w-1/5 xl:w-1/5 p-2">
+//           <div className="bg-slate-50 h-48 flex items-center justify-center m-2 rounded-lg shadow-md hover:shadow-lg transition duration-300 hover:bg-slate-200 hover:text-blue-500">
+//             <div className="text-center">
+//               <Link to="/">
+//                 <FontAwesomeIcon icon={faCreditCard} size="2x" />
+//                 <div className="text-xl items-center mt-2">To Self Account</div>
+//               </Link>
+//             </div>
+//           </div>
+//         </div>
+//         <div className="w-full md:w-1/2 lg:w-1/5 xl:w-1/5 p-2">
+//           <div className="bg-slate-50 h-48 flex items-center justify-center m-2 rounded-lg shadow-md hover:shadow-lg transition duration-300 hover:bg-slate-200 hover:text-blue-500">
+//             <div className="text-center">
+//               <Link to="/">
+//                 <FontAwesomeIcon icon={faHandHoldingDollar} size="2x" />
+//                 <div className="text-xl items-center mt-2">Check Balance</div>
+//               </Link>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </div>  
+//   );
+// };
