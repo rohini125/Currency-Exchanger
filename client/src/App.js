fa1ccdc169a78982ab51c3cd3987a23dc13de2d6
sakshi parkale <120358706+SakshiParkale@users.noreply.github.com>
2024-10-10 17:11:49 +0530

    update app.js file

--- client/src/App.js
+++ client/src/App.js
@@ -8,10 +8,18 @@ import CurrencyConverter from "./Pages/currency-converter.js";
 import Notifications from "./Pages/Notifications.js";
 import HelpPage from "./Pages/HelpPage.js";
 import Markets from "./Pages/Markets.js";
-// import TransactionMoney from "./Pages/TransactionMoney";
 import To_mobileNumber from "./Pages/To_mobileNumber.js";
 import To_Bank_UPI from "./Pages/To_Bank_UPI.js";
 import To_SelfAccount from "./Pages/To_SelfAccount.js";
+//////////////////
+import ProfilePage from "./Pages/ProfilePage.js";
+import Account from "./Pages/Account.js";
+import Settings from "./Pages/Settings.js";import ChangePassword from "./Pages/Changepassword.js";
+import SecurityQuestions from "./Pages/SecurityQuestions.js";
+import TwoFactorAuthentication from "./Pages/TwoFactorAuthentication.js";
+import ContactSupport from "./Pages/ContactSupport.js";
+import FAQs from "./Pages/FAQs.js";
+import Logout from "./Pages/Logout.js"
 
 function App() {
   return (
@@ -27,31 +35,7 @@ function App() {
       <Route path="/to-mobile-number" element={<To_mobileNumber />} />
       <Route path="/to-bank-upi" element={<To_Bank_UPI />} />
       <Route path="/to-self-account" element={<To_SelfAccount />} />
-    </Routes>
-    <Footer/>
-=======
-import ProfilePage from "./Pages/ProfilePage.js";
-import Account from "./Pages/Account.js";
-import Settings from "./Pages/Settings.js";import ChangePassword from "./Pages/Changepassword.js";
-import SecurityQuestions from "./Pages/SecurityQuestions.js";
-import TwoFactorAuthentication from "./Pages/TwoFactorAuthentication.js";
-import ContactSupport from "./Pages/ContactSupport.js";
-import FAQs from "./Pages/FAQs.js";
-import Logout from "./Pages/Logout.js"
-
-
-function App() {
-  return (
-    <div className="bg-gray-50 h-full w-full">
-      <Header />
-      <Routes>
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/" element={<CurrencyConverter />} />
-        <Route path="/markets" element={<Markets />} />
-        <Route path="/help" element={<HelpPage />} />
-        <Route path="/notifications" element={<Notifications />} />
-        <Route path="/profile" element={<ProfilePage />} />
+      <Route path="/profile" element={<ProfilePage />} />
         <Route path="/account" element={<Account />} />
         <Route path="/settings" element={<Settings />} />
         <Route path="/changepassword" element={<ChangePassword />} />
@@ -60,12 +44,14 @@ function App() {
         <Route path="/contact-support" element={< ContactSupport/>}/>
         <Route path="/faqs" element={<FAQs/>}/> 
         <Route path="/logout" element={<Logout/>}/>
-        
-      </Routes>
-      <Footer />
->>>>>>> e066e199561e9c864f2def22ed9ecd29bcbb6fbc
-    </div>
+    </Routes>
+    <Footer/>
+    </div> 
   );
 }
 
 export default App;
+
+
+
+
